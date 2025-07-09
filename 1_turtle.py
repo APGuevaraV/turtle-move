c3489224c2f9a3d38589fd7d9ca2091fca6c63f9
Ana Paula <ana.guevara.valladares@gmail.com>
2025-07-08 22:58:03 -0500

    moving the turtle

--- 1_turtle.py
+++ 1_turtle.py
@@ -5,19 +5,36 @@ window = turtle.Screen()
 window.title("Viaje con Fio")
 window.bgcolor("#eaefd7")
 window.setup(500,500)
+window.setworldcoordinates(0,500,500,0)
+
 
 #configuracion de la tortuga
 fio = turtle.Turtle()
 fio.shape('turtle')
 fio.color('darkgreen')
 fio.pensize(2)
-fio.speed(1)
+fio.speed(2)
 
 
 #instrucciones
-fio.forward(100)
-fio.left(90)
-fio.forward(150)
-fio.home()
+fio.penup()
+fio.goto(200,200)
+fio.pendown()
+fio.goto(350,200)
+fio.goto(350,350)
+fio.goto(200,350)
+fio.goto(200,200)
+print(fio.pos())
+
+fio.circle(25)
+fio.circle(50)
+fio.circle(75)
+fio.circle(150)
+
+for i in range(0,5):
+    fio.penup()
+    fio.goto(i*25,i*25)
+    fio.pendown()
+    fio.circle(i*25)
 
 turtle.mainloop()
